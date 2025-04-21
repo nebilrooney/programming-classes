ffb2236998e546e8dda11490151081abcc01e9f5
Nebil ferej <nabilrooney123@gmail.com>
2025-04-20 22:13:45 -0700

    Add toString

--- script.js
+++ script.js
@@ -11,4 +11,9 @@ getTotalValue() {
     return this.price * this.quantity;
   }
 
+
+toString() {
+    return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+  }
 }
+
